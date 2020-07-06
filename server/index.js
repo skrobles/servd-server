275b3cded8e7fa39e47a1c832ef7f2bfc1f2060a
shelilevine <shelisteinberg@gmail.com>
2020-07-06 17:47:29 -0400

    feature: sending static files from backend

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = new Koa()
 const Router = require('koa-router')
 const router = new Router()
 const mount = require('koa-mount')
+const path = require('path')
 const PORT = process.env.PORT || 8080
 const {getRecipes} = require('./spoonAPI')
 
@@ -14,12 +15,16 @@ module.exports = app
 // setup the logger
 app.use(morgan('dev'))
 
+//static middleware
+
 
 // API routes
 require('./routes')(router)
 // app.use(router.routes())
 app.use(mount('/api', router.routes()))
 
+
+app.use(require('koa-static')(path.join(__dirname, '..', 'build')))
 // app.use(async (ctx) => {
 //   console.log(ctx)
 //   const recipes = await getRecipes(['lettuce', 'tomato', 'parmesan cheese'])
