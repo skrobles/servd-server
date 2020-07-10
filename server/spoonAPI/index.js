254d94a8a0e724d3e493f61dfc74298da5814682
Raghdaa Barmo <raghdaa.barmo@gmail.com>
2020-07-10 13:29:11 -0400

    feat: real data renders on load

--- server/spoonAPI/index.js
+++ server/spoonAPI/index.js
@@ -1,57 +1,62 @@
-const {spoonApiKey} = require('../../secrets')
-const axios = require('axios')
-const testData = require('../../scripts/example.json')
-
+const { spoonApiKey } = require("../../secrets");
+const axios = require("axios");
+//const testData = require("../../scripts/example.json");
 
 const getRecipes = async (ingredientQuery) => {
-  const ingredientList = ingredientQuery.ingredients.split(', ')
-  const escapedIngredients = ingredientList.join('%2C ')
-  // const {data} = await axios({
-  //   "method":"GET",
-  //   "url":"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch",
-  //   "headers":{
-  //   "content-type":"application/octet-stream",
-  //   "x-rapidapi-host":"spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-  //   "x-rapidapi-key": spoonApiKey,
-  //   "useQueryString":true
-  //   },"params":{
-  //   "number":"2",
-  //   "includeIngredients": escapedIngredients,
-  //   "ranking":"1",
-  //   "addRecipeInformation": true
-  //   }
-  //   })
-    const data = testData
-    let ingredients
-    const recipes = data.results.map(result => {
-
-      //get list of ingredients without duplicates
-      ingredients = []
-      result.analyzedInstructions[0].steps.map(step => step.ingredients.forEach(ingredient => {
-        if (!ingredients.includes(ingredient.name)) {
-          ingredients.push(ingredient.name)
-        }
-      }))
+  const ingredientList = ingredientQuery.ingredients.split(", ");
+  const escapedIngredients = ingredientList.join("%2C ");
+  const { data } = await axios({
+    method: "GET",
+    url:
+      "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch",
+    headers: {
+      "content-type": "application/octet-stream",
+      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+      "x-rapidapi-key": spoonApiKey,
+      useQueryString: true
+    },
+    params: {
+      number: "2",
+      includeIngredients: escapedIngredients,
+      ranking: "1",
+      addRecipeInformation: true
+    }
+  });
+  // const data = testData
+  let ingredients;
+  let steps;
+  const recipes = data.results.map((result) => {
+    //get list of ingredients without duplicates
+    ingredients = [];
+    if (result.analyzedInstructions.length > 0) {
+      result.analyzedInstructions[0].steps.map((step) =>
+        step.ingredients.forEach((ingredient) => {
+          if (!ingredients.includes(ingredient.name)) {
+            ingredients.push(ingredient.name);
+          }
+        })
+      );
 
       //get steps for recipe
-      let steps = result.analyzedInstructions[0].steps.map(step => step.step)
+      steps = result.analyzedInstructions[0].steps.map((step) => step.step);
+    }
 
-      //formulate our recipe object
-      let recipe = {
-        title: result.title,
-        imgUrl: result.image,
-        ingredients,
-        steps,
-        sourceUrl: result.sourceUrl,
-        servings: result.servings,
-        time: result.readyInMinutes
-      }
-      return recipe
-    })
+    //formulate our recipe object
+    let recipe = {
+      title: result.title,
+      imgUrl: result.image,
+      ingredients,
+      steps,
+      sourceUrl: result.sourceUrl,
+      servings: result.servings,
+      time: result.readyInMinutes
+    };
+    return recipe;
+  });
 
-    // return recipes
-    return recipes
-}
+  // return recipes
+  return recipes;
+};
 
 //other params that might be useful
 //type: main course
@@ -60,8 +65,6 @@ const getRecipes = async (ingredientQuery) => {
 //intolerances
 //excludeIngredients
 
-
-
 module.exports = {
   getRecipes
-}
+};
